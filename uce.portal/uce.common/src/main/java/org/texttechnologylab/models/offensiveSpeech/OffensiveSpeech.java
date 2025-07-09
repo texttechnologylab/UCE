8ad779f9321c4539ebea961c2aa0504fdd03fd14
mirelamuza <s7375418@stud.uni-frankfurt.de>
2025-07-09 21:06:32 +0200

    Add offensive and non-offensive flags to OffensiveSpeech entity

--- uce.portal/uce.common/src/main/java/org/texttechnologylab/models/offensiveSpeech/OffensiveSpeech.java
+++ uce.portal/uce.common/src/main/java/org/texttechnologylab/models/offensiveSpeech/OffensiveSpeech.java
@@ -1,18 +1,31 @@
 package org.texttechnologylab.models.offensiveSpeech;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 import org.texttechnologylab.annotations.Typesystem;
 import org.texttechnologylab.models.UIMAAnnotation;
 import org.texttechnologylab.models.WikiModel;
 import org.texttechnologylab.models.corpus.Document;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "offensive-speech")
 @Typesystem(types = {org.texttechnologylab.annotation.OffensiveSpeech.class})
 public class OffensiveSpeech extends UIMAAnnotation implements WikiModel {
 
+    @Getter
+    @Setter
+    @Column(name = "offensive", nullable = false)
+    private boolean offensive;
 
+    @Getter
+    @Setter
+    @Column(name = "non_offensive", nullable = false)
+    private boolean nonOffensive;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JoinColumn(name = "document_id", nullable = false)
