cc2dda6b46c7fe0838f6448a1a11ecb929398a9e
mirelamuza <s7375418@stud.uni-frankfurt.de>
2025-07-09 20:57:14 +0200

    Add OffensiveSpeech entity for handling offensive speech annotations

---
+++ uce.portal/uce.common/src/main/java/org/texttechnologylab/models/offensiveSpeech/OffensiveSpeech.java
@@ -0,0 +1,45 @@
+package org.texttechnologylab.models.offensiveSpeech;
+
+import org.texttechnologylab.models.UIMAAnnotation;
+import org.texttechnologylab.models.WikiModel;
+import org.texttechnologylab.models.corpus.Document;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "offensive-speech")
+public class OffensiveSpeech extends UIMAAnnotation implements WikiModel {
+
+
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name = "document_id", nullable = false)
+    private Document document;
+
+    public OffensiveSpeech() {
+        super(-1, -1);
+    }
+
+    public OffensiveSpeech(int begin, int end) {
+        super(begin, end);
+    }
+
+    public OffensiveSpeech(int begin, int end, String coveredText) {
+        super(begin, end);
+        setCoveredText(coveredText);
+    }
+
+    public Document getDocument() {
+        return document;
+    }
+
+    public void setDocument(Document document) {
+        this.document = document;
+    }
+
+    @Override
+    public String getWikiId() {
+        return "UT" + "-" + this.getId();
+    }
+
+}
