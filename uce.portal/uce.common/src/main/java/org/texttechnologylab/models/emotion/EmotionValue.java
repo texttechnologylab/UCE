9b0a8b1778cecde9478f76768bf38906856feffb
UnleqitQ <quentin.weinbrenner@gmail.com>
2025-07-10 15:23:06 +0200

    Refactor EmotionValue class to extend UIMAAnnotation and implement WikiModel; add document association and constructors

--- uce.portal/uce.common/src/main/java/org/texttechnologylab/models/emotion/EmotionValue.java
+++ uce.portal/uce.common/src/main/java/org/texttechnologylab/models/emotion/EmotionValue.java
@@ -2,13 +2,15 @@ package org.texttechnologylab.models.emotion;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.texttechnologylab.models.ModelBase;
+import org.texttechnologylab.models.UIMAAnnotation;
+import org.texttechnologylab.models.WikiModel;
+import org.texttechnologylab.models.corpus.Document;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "emotion_value")
-public class EmotionValue extends ModelBase {
+public class EmotionValue extends UIMAAnnotation implements WikiModel {
 
     @Getter
     @Setter
@@ -27,12 +29,34 @@ public class EmotionValue extends ModelBase {
     @Column(name = "value", nullable = false)
     private double value;
 
+    @ManyToOne(fetch = FetchType.LAZY, optional = false, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name = "document_id", nullable = false)
+    private Document document;
+
     public EmotionValue() {
+        super(-1, -1);
     }
 
-    public EmotionValue(EmotionType emotionType, Emotion emotion, double value) {
-        this.emotionType = emotionType;
-        this.emotion = emotion;
-        this.value = value;
+    public EmotionValue(int begin, int end) {
+        super(begin, end);
     }
+
+    public EmotionValue(int begin, int end, String coveredText) {
+        super(begin, end);
+        setCoveredText(coveredText);
+    }
+
+    public Document getDocument() {
+        return document;
+    }
+
+    public void setDocument(Document document) {
+        this.document = document;
+    }
+
+    @Override
+    public String getWikiId() {
+        return "EV" + "-" + this.getId();
+    }
+
 }
