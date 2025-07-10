d2f33664a7f530486abf0b6e8f5ef84911104df8
imer <s8408248@uni-frankfurt.de>
2025-07-10 16:00:02 +0200

    New Columns and get set methods for columns added

--- uce.portal/uce.common/src/main/java/org/texttechnologylab/models/sentiment/Sentiment.java
+++ uce.portal/uce.common/src/main/java/org/texttechnologylab/models/sentiment/Sentiment.java
@@ -1,5 +1,7 @@
 package org.texttechnologylab.models.sentiment;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.texttechnologylab.models.UIMAAnnotation;
 import org.texttechnologylab.models.WikiModel;
 import org.texttechnologylab.models.corpus.Document;
@@ -9,6 +11,27 @@ import javax.persistence.*;
 @Entity
 @Table(name = "sentiment")
 public class Sentiment extends UIMAAnnotation implements WikiModel {
+
+    @Getter
+    @Setter
+    @Column(name = "sentiment", nullable = false)
+    private int sentiment;
+
+    @Getter
+    @Setter
+    @Column(name = "probability_positive", nullable = false)
+    private double probabilityPositive;
+
+    @Getter
+    @Setter
+    @Column(name = "probability_neutral", nullable = false)
+    private double probabilityNeutral;
+
+    @Getter
+    @Setter
+    @Column(name = "probability_negative", nullable = false)
+    private double probabilityNegative;
+
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JoinColumn(name = "document_id", nullable = false)
