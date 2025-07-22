8c70e1668ce77c8685334276c6a9f10180a8421c
UnleqitQ <quentin.weinbrenner@gmail.com>
2025-07-22 16:47:23 +0200

    Add many-to-many relationship between Model and ModelCategory

--- uce.portal/uce.common/src/main/java/org/texttechnologylab/models/modelInfo/ModelCategory.java
+++ uce.portal/uce.common/src/main/java/org/texttechnologylab/models/modelInfo/ModelCategory.java
@@ -2,11 +2,15 @@ package org.texttechnologylab.models.modelInfo;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 import org.texttechnologylab.models.ModelBase;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import java.util.Set;
 
 @Entity
 @Table(name = "model_category")
@@ -16,6 +20,12 @@ public class ModelCategory extends ModelBase {
     @Setter
     @Column(name = "category_name", nullable = false, unique = true)
     private String categoryName;
+
+    @Getter
+    @Setter
+    @ManyToMany(mappedBy = "categories")
+    @Fetch(FetchMode.JOIN)
+    private Set<Model> models;
 
     public ModelCategory() {
         // Default constructor for JPA
