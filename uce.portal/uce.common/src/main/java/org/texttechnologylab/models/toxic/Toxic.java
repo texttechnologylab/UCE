ea4e33f52671f719da425f7d3e9dfcc4c708e612
Xinran Wang <s1922410@stud.uni-frankfurt.de>
2025-07-25 18:56:37 +0200

    Add ToxicValue and ToxicType classes with relationships to Toxic

--- uce.portal/uce.common/src/main/java/org/texttechnologylab/models/toxic/Toxic.java
+++ uce.portal/uce.common/src/main/java/org/texttechnologylab/models/toxic/Toxic.java
@@ -1,20 +1,27 @@
 package org.texttechnologylab.models.toxic;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 import org.texttechnologylab.annotations.Typesystem;
 import org.texttechnologylab.models.UIMAAnnotation;
 import org.texttechnologylab.models.WikiModel;
 import org.texttechnologylab.models.corpus.Document;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "toxic")
 @Typesystem(types = {org.texttechnologylab.annotation.Toxic.class})
 public class Toxic extends UIMAAnnotation implements WikiModel {
-    @Column(name = "toxic", nullable = false)
-    private double toxic;
-    @Column(name = "non_toxic", nullable = false)
-    private double nonToxic;
+
+    @Getter
+    @Setter
+    @OneToMany(mappedBy = "toxic", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @Fetch(value = FetchMode.SUBSELECT)
+    private List<ToxicValue> toxicValues;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JoinColumn(name = "document_id", nullable = false)
@@ -31,21 +38,6 @@ public class Toxic extends UIMAAnnotation implements WikiModel {
     public Toxic(int begin, int end, String coveredText) {
         super(begin, end);
         setCoveredText(coveredText);
-    }
-
-    public double getToxic() {
-        return toxic;
-    }
-    public void setToxic(double newToxic) {
-        this.toxic = newToxic;
-    }
-
-    public double getNonToxic() {
-        return nonToxic;
-    }
-
-    public void setNonToxic(double nonToxic) {
-        this.nonToxic = nonToxic;
     }
 
     public Document getDocument() {
