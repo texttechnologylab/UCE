8e4afbf13d62e05ec4376ea1ae2abdcf02210ff8
imer <s8408248@uni-frankfurt.de>
2025-08-13 16:20:33 +0200

    Dynamic Sentiment Types

--- uce.portal/uce.common/src/main/java/org/texttechnologylab/models/sentiment/Sentiment.java
+++ uce.portal/uce.common/src/main/java/org/texttechnologylab/models/sentiment/Sentiment.java
@@ -2,12 +2,15 @@ package org.texttechnologylab.models.sentiment;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 import org.texttechnologylab.annotations.Typesystem;
 import org.texttechnologylab.models.UIMAAnnotation;
 import org.texttechnologylab.models.WikiModel;
 import org.texttechnologylab.models.corpus.Document;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "sentiment")
@@ -21,19 +24,9 @@ public class Sentiment extends UIMAAnnotation implements WikiModel {
 
     @Getter
     @Setter
-    @Column(name = "probability_positive", nullable = false)
-    private double probabilityPositive;
-
-    @Getter
-    @Setter
-    @Column(name = "probability_neutral", nullable = false)
-    private double probabilityNeutral;
-
-    @Getter
-    @Setter
-    @Column(name = "probability_negative", nullable = false)
-    private double probabilityNegative;
-
+    @OneToMany(mappedBy = "sentiment", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @Fetch(value = FetchMode.SUBSELECT)
+    private List<SentimentValue> sentimentValues;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JoinColumn(name = "document_id", nullable = false)
